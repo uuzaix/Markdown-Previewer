c7bc7159cd776f15e95c09e7872cc7c41a538f69
Larisa Dolgova <larisa.dolgova@gmail.com>
2017-02-16 22:21:52 +0100

    Add default input. Move style to css file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import marked from 'marked';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = {
+      value: "Markdown Previewer\n=====\n\nTry it out!\n----\n\n*This text will be italic* \n\n**This text will be bold**\n\nYou can create lists:\n\n * like\n * this\n\nFor more examples read [GitHub Guide](https://guides.github.com/features/mastering-markdown)" };
 
     this.handleChange = this.handleChange.bind(this);
   }
@@ -15,10 +16,11 @@ class App extends React.Component {
 
   render() {
     return (
-      <div style={{ display: 'flex' }}>
-        <textarea name="markdown" id="userInput" cols="70" rows="30" onChange={this.handleChange} style={{ margin: '20px' }}></textarea>
-        <div id="output" dangerouslySetInnerHTML={{ __html: marked(this.state.value) }}></div>
+      <div className='container'>
+        <textarea className='box' name="markdown" id="userInput" rows="20" onChange={this.handleChange} value={this.state.value}></textarea>
+        <div id="output" className='box' dangerouslySetInnerHTML={{ __html: marked(this.state.value) }}></div>
       </div>)
   }
 }
+
 export default App;
